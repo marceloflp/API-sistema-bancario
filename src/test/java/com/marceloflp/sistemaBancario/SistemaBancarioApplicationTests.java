8307caa305aca29d07c45374ed1db908c151d5df
marceloflp <mfelipedasilva2020@gmail.com>
2026-05-24 13:17:29 -0300

    Criacao do projeto

---
+++ src/test/java/com/marceloflp/sistemaBancario/SistemaBancarioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marceloflp.sistemaBancario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaBancarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
